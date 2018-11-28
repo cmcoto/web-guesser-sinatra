4e6d84f18ca9698b41bf5381571afb734bdb5260
Carlos Coto <cmcoto@gmail.com>
2018-11-28 19:27:12 +0000

    Worked on Messages for numbers

--- web-guesser.rb
+++ web-guesser.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = 1 + rand(99)
+SECRET_NUMBER = 1 + rand(99)
 guesses_left = 10
 correct = false
 
@@ -9,9 +9,28 @@ correct = false
 
 get '/' do
   #throw params.inspect 
-  guess = params["guess"]
-  erb :index, :locals => {:number => number}
+  
+  message = check_guess(params["guess"])
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 
   #erb :index
 end
 
+def check_guess(guess)
+	if guess.nil?
+		return "Pick a number 1-100"
+	else
+		if guess.to_i > SECRET_NUMBER + 5 
+	  		return "Whoa! Way Too High"
+	    elsif guess.to_i < SECRET_NUMBER - 5 
+	  		return "Yikes! Way Too Low"
+	 	elsif guess.to_i > SECRET_NUMBER 
+	  		return "High...But Almost!" 
+	  	elsif guess.to_i < SECRET_NUMBER 
+	  		return "Low... But Almost!"
+	    elsif  guess.to_i == SECRET_NUMBER 
+	    	correct = true
+	  		return "CONGRATS! You have guessed correctly!"
+	  	end 
+	end
+end
